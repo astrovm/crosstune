861aa36e9826fd8e0e84dfab068472744ecd3cfe
astrovm <~@4st.li>
2026-02-15 18:28:27 -0300

    feat: Update Android compile, min, and target SDK versions to 36 and 26.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.astrolabs.musicurlhandler"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.astrolabs.musicurlhandler"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 26
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
